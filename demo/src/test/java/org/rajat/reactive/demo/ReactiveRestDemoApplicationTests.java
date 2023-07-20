da182350745478fd96ae8018e932606046c090ef
Rajat Dey <giet13me131@gmail.com>
2023-07-20 14:36:29 +0530

    Added reactive demo starter skeletal code

---
+++ demo/src/test/java/org/rajat/reactive/demo/ReactiveRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rajat.reactive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
